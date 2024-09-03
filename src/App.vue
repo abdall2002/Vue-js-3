1e8a307299756c5a9f65a3e7a29d7683cbed2358
unknown <abdallahaliahmed517@gmail.com>
2024-09-03 14:05:44 +0300

    KKK

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <app-header />
+        <!-- <app-footer/> -->
+        <div class="container">
+            <button @click="activeComp = Mike">Mike</button>
+            <button @click="activeComp = Steve">Steve</button>
+            <!-- <Mike v-if="activeComp === Mike" /> -->
+            <!-- <Steve v-if="activeComp === Steve" /> -->
+            <!-- <component :is="activeComp"></component> -->
+            <keep-alive include="mike,steve">
+                <component :is="activeComp"></component>
+            </keep-alive>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { ref, shallowRef, reactive, provide } from 'vue';
+    import Mike from '@/components/Players/mike.vue';
+    import Steve from '@/components/Players/steve.vue';
+
+    const activeComp = shallowRef(Mike);
+
+</script>
+
+<style>
+    body {
+        padding: 0;
+        margin: 0;
+        font-family: 'Roboto', sans-serif;
+    }
+    .container {
+        min-height: 84vh;
+        box-sizing: border-box;
+        padding: 20px;
+    }
+
+</style> 
---
+++ src/components/Players/steve.vue
@@ -0,0 +1,19 @@
+<template>
+    <h3>Steve is on</h3>
+</template>
+
+<!-- <script setup>
+    import { onUnmounted } from 'vue'
+
+    onUnmounted(() => {
+        console.log('Steve unmounted')
+    })
+</script> -->
+
+<script setup>
+import { onUnmounted } from 'vue';
+
+onUnmounted(()=>{
+    console.log('Steve unmounted')
+})
+</script>
